bc4ec2e1b42043594f7bc142e4bd0cf061ff6452
Haye <thomas@tmq.be>
2024-01-03 16:05:39 +0100

    changed route protection

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
                 .authorizeExchange(exchange ->
-                        exchange.pathMatchers(HttpMethod.GET,"/bookings")
+                        exchange.pathMatchers(HttpMethod.GET,"/hotels")
                                 .permitAll()
                                 .anyExchange()
                                 .authenticated()
