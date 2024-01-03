0821b31eb0733ae2ce14596278ce11613027582f
Haye <thomas@tmq.be>
2024-01-03 18:10:11 +0100

    id error

--- booking-service/src/main/java/fact/it/bookingservice/dto/BookingResponse.java
+++ booking-service/src/main/java/fact/it/bookingservice/dto/BookingResponse.java
@@ -10,7 +10,6 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookingResponse {
-    private Long id;
     private String bookingNbr;
     private Long hotelId;
     private Long customerId;
