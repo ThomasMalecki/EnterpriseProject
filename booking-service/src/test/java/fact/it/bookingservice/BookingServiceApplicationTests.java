96a5b0c7d801295a05ce0ec6fbc7b4713985d24f
Haye <thomas@tmq.be>
2024-01-04 21:34:32 +0100

    unit test fix attempt

--- booking-service/src/test/java/fact/it/bookingservice/BookingServiceApplicationTests.java
+++ booking-service/src/test/java/fact/it/bookingservice/BookingServiceApplicationTests.java
@@ -1,21 +1,27 @@
 package fact.it.bookingservice;
 import fact.it.bookingservice.dto.BookingRequest;
 import fact.it.bookingservice.dto.BookingResponse;
+import fact.it.bookingservice.dto.CustomerResponse;
 import fact.it.bookingservice.model.Booking;
 import fact.it.bookingservice.repository.BookingRepository;
 import fact.it.bookingservice.service.BookingService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -27,20 +33,51 @@ public class BookingServiceApplicationTests {
 	@Mock
 	private WebClient webClient; // Mock external dependencies
 
+
+	@Mock
+	private WebClient.RequestHeadersUriSpec requestHeadersUriSpec;
+
+	@Mock
+	private WebClient.RequestHeadersSpec requestHeadersSpec;
+
+	@Mock
+	private WebClient.ResponseSpec responseSpec;
+
 	@Mock
 	private BookingRepository bookingRepository; // Mock database repository
 
+
+
+	@BeforeEach
+	void setUp() {
+		ReflectionTestUtils.setField(bookingService, "customerServiceBaseUrl", "http://localhost:8082");
+	}
 	@Test
 	public void placeBooking_ValidRequest_ReturnsTrue() throws Exception {
 		BookingRequest bookingRequest = new BookingRequest(1L, 2L, 3);
 
+		CustomerResponse customerResponse = new CustomerResponse();
+		customerResponse.setEmail("test@test.be");
+		customerResponse.setFirstName("test");
+		customerResponse.setLastName("testlast");
+		customerResponse.setPhone("44848454989");
+		Booking booking = new Booking(1L ,UUID.randomUUID().toString(),1L, 2L, 3);
+
 		when(bookingRepository.save(any())).thenReturn(new Booking());
+
+		when(bookingRepository.save(any(Booking.class))).thenReturn(booking);
+
+		when(webClient.get()).thenReturn(requestHeadersUriSpec);
+		when(requestHeadersUriSpec.uri(anyString(),  any(Function.class))).thenReturn(requestHeadersSpec);
+		when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
+		when(responseSpec.bodyToMono(CustomerResponse[].class)).thenReturn(Mono.just(new CustomerResponse[]{customerResponse}));
+
 
 		boolean result = bookingService.placeBooking(bookingRequest);
 
-		assert result; // Check that booking was successful
+		assertTrue(result);
 
-		verify(bookingRepository, times(1)).save(any()); // Verify that save method was called
+		verify(bookingRepository, times(1)).save(any(Booking.class));
 	}
 
 	@Test
