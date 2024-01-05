362ae2791c4203609b3ec52a8e047abd634e52c4
Haye <thomas@tmq.be>
2024-01-05 15:33:33 +0100

    added check for customers exists

--- booking-service/src/main/java/fact/it/bookingservice/service/BookingService.java
+++ booking-service/src/main/java/fact/it/bookingservice/service/BookingService.java
@@ -29,6 +29,17 @@ public class BookingService {
 
     public boolean placeBooking(BookingRequest bookingRequest) {
 
+
+        CustomerResponse customerResponse = webClient.get()
+                .uri("http://" + customerServiceBaseUrl + "/api/customer/by-id",
+                        uriBuilder -> uriBuilder.queryParam("customerId", bookingRequest.getCustomerId()).build())
+                .retrieve()
+                .bodyToMono(CustomerResponse.class)
+                .block();
+
+        if(customerResponse == null){
+            return false;
+        }
         Booking booking = new Booking();
         booking.setBookingNbr(UUID.randomUUID().toString());
 
